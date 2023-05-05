31a65fe7afd424eec605c4d8b730655e0a54d6b6
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-04 20:44:46 -0700

    Complete get_project_by_title function

--- hackbright.py
+++ hackbright.py
@@ -60,7 +60,18 @@ def make_new_student(first_name, last_name, github):
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+
+    QUERY = """
+    SELECT description,max_grade 
+    FROM projects
+    WHERE title = :title
+    """
+
+    db_cursor = db.session.execute(QUERY, {'title':title})
+    
+    row = db_cursor.fetchone()
+
+    print(f"Project: {title}\nDescription: {row[0]}\nMax grade: {row[1]}")
 
 
 def get_grade_by_github_title(github, title):
@@ -95,6 +106,10 @@ def handle_input():
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+        
+        elif command == "project_info":
+            title = args[0]
+            get_project_by_title(title)
 
         else:
             if command != "quit":
